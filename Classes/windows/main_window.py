387546ab3794ad73205bea7bf49bf9f811d7dcff
Puppyroni <puppers2022pepperoni@gmail.com>
2024-03-16 18:55:05 +0000

    Image setup not working for now

--- Classes/windows/main_window.py
+++ Classes/windows/main_window.py
@@ -1,6 +1,8 @@
 # imports
 from tkinter import *
 from tkinter import Tk
+from tkinter import PhotoImage
+from PIL import Image, ImageTk
 from Classes.windows.register_window import RegisterWindow
 from Classes.windows.login_window import LogInWindow
 
@@ -12,6 +14,25 @@ class MainWindow:
         self.main_window.title('Card Reader') # Change Tittle
         self.main_window.iconbitmap('Assets/icons/icon.ico') # Change icon
         self.main_window.configure(bg = '#f0f0f0') # Change the background color
+        
+        # Set width and height of the window 
+        # Off for now
+        #width = self.main_window.winfo_screenwidth()
+        #height = self.main_window.winfo_screenheight()
+        #alignstr = '%dx%d+0+0' % (width, height)
+        #self.main_window.geometry(alignstr)
+
+        # Create Image Background
+        # P: Cannat get the image to load idk know why
+        try:
+            image = Image.open("Assets/image/retrato-de-um-trabalhador-loiro-sorridente-tomando-um-legumes_13339-149292.jpg")
+            tk_image = ImageTk.PhotoImage(image)
+            image_lbl = Label(self.main_window, image=tk_image)
+            image_lbl.place(x=0, y=0)
+        except FileNotFoundError:
+            print("Arquivo de imagem não encontrado.")
+        except TclError:
+            print("Erro ao carregar a imagem. Verifique se o formato da imagem é suportado.")
         
         # Configure a welcome text
         self.welcome_lbl = Label(self.main_window, text = 'Card Reader', font = 'Arial 20', bg = '#f0f0f0')
